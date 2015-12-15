d94ba1e74b83c493b4404001567c920ad3fa96aa
Alexander <alexander@alexander-ThinkPad-X1-Carbon-3rd>
2015-12-15 12:47:10 +0100

    BA version of monitoring

---
+++ Schreibtisch/genode/repos/ferret/src/ferret/examples/scalarmonitor/main.cc
@@ -0,0 +1,41 @@
+#include <base/env.h>
+#include <base/printf.h>
+#include <sensordir_session/client.h>
+#include <sensordir_session/connection.h>
+#include <dataspace/client.h>
+#include <sensors/scalar.h>
+#include <timer_session/connection.h>
+#include <monitor.h>
+
+using namespace Genode;
+
+int main(void)
+{
+	Timer::Connection t;
+
+	t.msleep(2000);
+
+	printf("Hello from the scalar monitor.\n");
+
+	ferret_scalar_t * s1 = NULL, * s2 = NULL, * s3 = NULL, * s4 = NULL;
+
+	ferret_att(10, 1, 0, s1);
+
+	ferret_att(20, 1, 0, s2);
+
+	ferret_att(30, 1, 0, s3);
+
+	ferret_att(40, 1, 0, s4);
+
+
+	while (1) {
+		printf("***************************\n");
+		printf("%d\n", s1->data);
+		printf("%d\n", s2->data);
+		printf("%d\n", s3->data);
+		printf("%d\n", s4->data);
+		t.msleep(500);
+	}
+
+    return 0;
+}
